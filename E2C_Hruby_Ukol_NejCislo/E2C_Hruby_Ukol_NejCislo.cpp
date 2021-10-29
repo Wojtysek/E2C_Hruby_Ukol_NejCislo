773602b13788946b0b4860215bf70c0f4834d44f
Nitro <Nitro@LAPTOP-N9TP630A>
2021-10-29 16:14:25 +0200

    Přidejte soubory projektu.

---
+++ E2C_Hruby_Ukol_NejCislo/E2C_Hruby_Ukol_NejCislo.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int a = 2, b = 6, c = 7;
+	int max;
+	int min;
+
+	if (a == b) {
+		min = a;
+	}
+	else {
+		if (a < b) {
+			min = b;
+		}
+		else {
+			min = a;
+		}
+	}
+	if (min == c) {
+		max = c;
+	}
+	else {
+		if (min < c) {
+			max = c;
+		}
+		else {
+			max = min;
+		}
+	}
+	printf("nejvetsi cislo je %d\n", max);
+}
